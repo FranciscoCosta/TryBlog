529c0a45788d980aa7024b9c521a8604791e8b5d
Xico <francisco100eg@gmail.com>
2022-12-07 16:00:51 -0300

    Build: Create Routes using React-router-dom v6.4

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,39 @@ import {
   RouterProvider,
   Route,
 } from "react-router-dom";
+import { Home } from "./Pages/Home";
+import { Login } from "./Pages/Login";
+import { Register } from "./Pages/Register";
+import { Single } from "./Pages/Single";
+import { Write } from "./Pages/Write";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+  {
+    path: "/single",
+    element: <Single/>,
+  },
+  {
+    path: "/",
+    element: <Write/>,
+  },
+]);
 
 function App() {
   return (
     <div>
-      Hello world!
+      <RouterProvider router={router} />
     </div>
   );
 }
