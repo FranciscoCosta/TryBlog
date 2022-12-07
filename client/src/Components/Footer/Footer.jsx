9a2e4476b48604e5f39ccd82f91d212d5b9d244e
Xico <francisco100eg@gmail.com>
2022-12-07 20:54:21 -0300

    Feat: Added Footer component and styles

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import logo from '../../assets/tryblog.png'
+import './Footer.scss'
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='Footer'>
+      <div className='Footer__logo'>
+        <img src={logo} alt="logo tryblog" />
+      </div>
+        <span>Made by : Francisco Costa T24A</span>
+    </footer>
   )
 }
