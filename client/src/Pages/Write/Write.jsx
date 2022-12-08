700556fcf6b04efb9cb4fac704cb653b45a806b4
Xico <francisco100eg@gmail.com>
2022-12-08 07:19:09 -0300

    Feat: Added dependencie react-quill

--- client/src/Pages/Write/Write.jsx
+++ client/src/Pages/Write/Write.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import { Footer } from '../../Components/Footer/Footer'
 import { Header } from '../../Components/Header/Header'
+import './Write.scss'
 
 export const Write = () => {
   return (
     <div>
       <Header/>
+      <div className='Write'>
+        <div className="Write__content">
+          Content
+        </div>
+        <div className="Write__menu">
+          <div className="Write__item1">i1</div>
+          <div className="Write__item1">i2</div>
+        </div>
+      </div>
       <Footer/>
     </div>
   )
