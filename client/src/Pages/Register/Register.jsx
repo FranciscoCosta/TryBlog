82446782013122c16b54a22bc555c8fca81d6d48
Xico <francisco100eg@gmail.com>
2022-12-11 14:00:34 -0300

    Feat: Added axios dependencie and working in register function

--- client/src/Pages/Register/Register.jsx
+++ client/src/Pages/Register/Register.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Link } from 'react-router-dom'
 import './Register.scss'
 
 export const Register = () => {
+  const [inputs, setInputs] = useState({
+    username:"",
+    email:"",
+    password:"",
+  })
+
+  const handleChange =(event)=>{
+    setInputs(oldstate=>({...oldstate, [event.target.name]: event.target.value }))
+  }
+  console.log(inputs)
   return (
     <div className='Register auth'>
       <h1>Register</h1>
       <form>
-        <input  required type="text" placeholder='username'/>
-        <input  required type="email" placeholder='email'/>
-        <input  required type="password" placeholder='password'/>
+        <input  required type="text" placeholder='username' name='username' onChange={handleChange}/>
+        <input  required type="email" placeholder='email' name="email" onChange={handleChange}/>
+        <input  required type="password" placeholder='password' name="password" onChange={handleChange}/>
         <button>Register</button>
         <p>Error!</p>
         <div className='Login__no-acount'>
